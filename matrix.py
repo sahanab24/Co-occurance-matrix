94fb4a2fe4f77c7af665a1e53a427672ec1a3669
Sudheer1995 <sudheerachary115@gmail.com>
2019-02-11 22:47:17 +0530

    add forward co-occurence

--- matrix.py
+++ matrix.py
@@ -1,14 +1,14 @@
-corpus=["abc def ijk pqr", "pqr klm opq", "lmn pqr xyz abc def pqr abc"]
-features=["abc", "pqr","def"]
-window_size= 2
-#Co_occurace matrix
+corpus = ["abc def ijk pqr", "pqr klm opq", "lmn pqr xyz abc def pqr abc"]
+features = ["abc", "pqr","def"]
 
-      #abc pqr	def
-#abc: 3 2	3
-#pqr: 2	3	2
-#def: 3	2	2
+# Co_occurace matrix
+      # abc pqr	def
+# abc:  3   2	3
+# pqr:  2   3	2
+# def:  3   2	2
+
 import numpy as np
-window=2
+window = 2
 C = np.zeros([len(features), len(features)])
 for feature in features:
       for review in corpus:
@@ -17,12 +17,8 @@ for feature in features:
                   indices = [i for i, x in enumerate(words) if x == feature]
                   for index in indices:
                         for j, word in enumerate(words):
-                              if j>i and word in features and abs(j-index) <= window :
-                                    #print(review)
-                                    #print(abs(j-index))
+                              if j >= index and word in features and abs(j-index) <= window:
                                     i = features.index(feature)
                                     j = features.index(word)
-                                    #print(i,feature,j,word)
-                                    #print("________________")
-                                    C[i][j] += 1                           
+                                    C[i][j] += 1
 print(C)
